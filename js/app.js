9d0edc203b665c36fe5fe35455d12734bb71433c
Alexander Lindqvist <alexander@solitudedesign.se>
2024-11-17 13:38:43 +0100

    Add functionally to remove alert message

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Event listner to the alert icon that remvoes message
+const alertBtn = document.getElementById('alertBtn').addEventListener('click', (e) => {
+    const alertMessage = document.getElementById('alertMessage').style.display = 'none';
+});
+
